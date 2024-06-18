2663620a598a63f407b3009bf0d62e1a5ced6580
rezaputra <rezaputramardiansyah@gmail.com>
2024-06-18 16:32:52 +0700

    Feat - Add variable rules

--- app.py
+++ app.py
@@ -3,11 +3,27 @@ from markupsafe import escape
 
 app = Flask(__name__)
 
-@app.get('/')
+@app.route('/')
 def index():
-    return "Hallo world"
+    return "Hello world!"
 
 # Html escaping
-@app.get('/hello/<name>')
+@app.route('/hello/<name>')
 def hello(name):
-    return f'Hello {escape(name)}'
+    return f'Hello {escape(name)}!'
+
+
+# Variable rules
+@app.route('/users/<username>')
+def show_user_profile(username):
+    return f'user {escape(username)}'
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    return f'Post id = {post_id}'
+
+@app.route('/path/<path:subpath>')
+def show_subpath(subpath):
+    return f'Subpath = {escape(subpath)}'
+
+
