535b0e5872ad121f9b2659cdbff2da9194fb7ce9
rezaputra <rezaputramardiansyah@gmail.com>
2024-06-18 16:16:37 +0700

    Feat - Add example of html escaping

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
 from flask import Flask
+from markupsafe import escape
 
 app = Flask(__name__)
 
 @app.get('/')
-def hello_world():
+def index():
     return "Hallo world"
+
+# Html escaping
+@app.get('/hello/<name>')
+def hello(name):
+    return f'Hello {escape(name)}'
