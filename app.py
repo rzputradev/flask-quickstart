f1374202caae506025be823b0132b89925f69c20
rezaputra <rezaputramardiansyah@gmail.com>
2024-06-18 16:45:01 +0700

    Feat - add url building

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, url_for
 from markupsafe import escape
 
 app = Flask(__name__)
@@ -27,3 +27,16 @@ def show_subpath(subpath):
     return f'Subpath = {escape(subpath)}'
 
 
+# Url building
+@app.route('/login')
+def login():
+    return 'Login'
+
+@app.route('/user/<username>')
+def profile(username):
+    return f'{username}\'s profile'
+
+with app.test_request_context():
+    print(url_for('login'))
+    print(url_for('login', next='/'))
+    print(url_for('profile', username='Reza'))
